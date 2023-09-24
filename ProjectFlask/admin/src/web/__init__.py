baa53e1fdeff6be53704d34dc64c4b44e3882e98
Vargas Rondal Cristian Gustavo <cristianvargasplus@gmail.com>
2023-09-23 23:36:53 -0300

    agrego modulo de configuracion

--- ProjectFlask/admin/src/web/__init__.py
+++ ProjectFlask/admin/src/web/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from src.web import error
 from src.web.controllers import issues  # <-- importamos el blueprint issues
+from src.web.config import config  # <-- importamos la configuración
 
 
 def create_app(
@@ -8,6 +9,7 @@ def create_app(
     static_folder="../../static",  # <-- definimos la carpeta de archivos estáticos
 ):
     app = Flask(__name__, static_folder=static_folder)  # <-- crea la aplicación
+    app.config.from_object(config[env])  # <-- setea la configuración
 
     app.register_blueprint(issues.issue_bp)  # <-- registramos el blueprint
 
@@ -15,12 +17,12 @@ def create_app(
     def home():
         return render_template("home.html")  # <-- renderizamos el template home.html
 
-    def sarasa():
-        return "sarasa"
-
-    app.add_url_rule("/sarasa", view_func=sarasa)  # <-- forma antigua de agregar rutas
-
     app.register_error_handler(
         404, error.not_found_error
     )  # <-- registramos el error 404
     return app
+
+    # def sarasa():
+    #     return "sarasa"
+
+    # app.add_url_rule("/sarasa", view_func=sarasa)  # <-- forma antigua de agregar rutas
---
+++ ProjectFlask/admin/src/web/config.py
@@ -0,0 +1,35 @@
+""" Modulo de configuración de la aplicación """
+
+
+class Config(object):
+    """Base de configuración"""
+
+    SECRET_KEY = "SECRET"
+    TESTING = False
+    SESSION_TYPE = "filesystem"
+
+
+class ProductionConfing(Config):
+    """Configuración de producción"""
+
+    pass
+
+
+class DevelopmentConfig(Config):
+    """Configuración de desarrollo"""
+
+    pass
+
+
+class TestingConfig(Config):
+    """Configuración de testing"""
+
+    TESTING = True
+    pass
+
+
+config = {
+    "development": DevelopmentConfig,
+    "production": ProductionConfing,
+    "test": TestingConfig,
+}
