9b01a5a79e87813593f1b76711059d68138106d4
Vargas Rondal Cristian Gustavo <cristianvargasplus@gmail.com>
2023-10-14 14:27:21 -0300

    agrego flask-session

--- ProjectFlask/admin/src/web/__init__.py
+++ ProjectFlask/admin/src/web/__init__.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask_session import Session
 from src.web import error
 from src.web.config import config  # <-- importamos la configuración
 from src.core import database  # <-- importamos la base de datos
 from src.web import routes  # <-- importamos las rutas
 from src.web import commands  # <-- importamos los comandos
+
+session = Session()
 
 
 def create_app(
@@ -12,6 +16,7 @@ def create_app(
 ):
     app = Flask(__name__, static_folder=static_folder)  # <-- crea la aplicación
     app.config.from_object(config[env])  # <-- setea la configuración
+    session.init_app(app)  # <-- inicializamos la sesión de flask_session
     database.init_app(app)  # <-- inicializamos la base de datos
     commands.register(app)  # <-- registramos los comandos
     routes.register(app)  # <-- registramos las rutas
